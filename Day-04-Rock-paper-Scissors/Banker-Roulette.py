27f365efa9d1f1fa04e68389a2bd33d152019774
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-03 18:11:01 +0530

    Day-4-The-Banker-Roulette

---
+++ Day-04-Rock-paper-Scissors/Banker-Roulette.py
@@ -0,0 +1,15 @@
+import random
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+# n = random.randint(0,4)
+#
+# if n == 0 :
+#     print("Alice will pay the bill")
+# if n == 1:
+#     print("Bob will pay the bill")
+# if n == 2:
+#     print("Charlie will pay the bill")
+# if n == 3:
+#     print("David will pay the bill")
+# if n == 4:
+#     print("Emanuel will pay the bill")
+print(random.choice(friends))
