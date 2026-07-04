59e20ca79c85e7d49d60b11bcc0b085574d64b80
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-04 16:24:54 +0530

    Day-8-Love-Calculator

---
+++ Day-08-Caesar-Cipher/LoveCalculator.py
@@ -0,0 +1,32 @@
+def calculate_love_score(name1, name2):
+    combined = (name1 +name2).lower()
+    true = 0
+    love = 0
+
+
+    for t in combined:
+        if t == "t":
+            true+= 1
+
+        if t == "r":
+            true+=1
+        if t == "u":
+            true+= 1
+        print(t)
+        if t == "e":
+            true+=1
+        if t == "l":
+            love+= 1
+
+        if t == "o":
+            love+=1
+        if t == "v":
+            love+= 1
+        print(t)
+        if t == "e":
+            love+=1
+score = str(true) + str(love)
+print(score)
+
+
+calculate_love_score("KanyeWest", "KimKardashinan")
