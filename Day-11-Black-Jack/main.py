9ba0e22dddd432f813107d7285d391cf3ad13c4c
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-07 16:21:17 +0530

    Made some changes in the game logic also added comments

--- Day-11-Black-Jack/main.py
+++ Day-11-Black-Jack/main.py
@@ -9,19 +9,22 @@ def deal_card(weight):
     return random.choices(cards,k = weight) # here I have written this in  way
 
 def calculate_score(got_cards):
-    if len(got_cards) == 2 and sum(got_cards) == 21:
+    """This function is used to calculate the score of the user and the dealer. """
+    if len(got_cards) == 2 and sum(got_cards) == 21: # If BlackJack is there then return 0 which is used to check in the main function
         return 0
+    # If the total is greater than 21 and there is an ace in the cards then change the value of ace from 11 to 1
     if sum(got_cards) > 21 and 11 in got_cards:
         got_cards.remove(11)
         got_cards.append(1)
     return sum(got_cards)
 def compare(user_score,dealer_score):
+    """This function is used to compare the score of the user and the dealer and print the result. """
     if user_score > 21:
-        print("Busted")
+        print("User Busted")
     elif dealer_score > 21:
         print("Dealer Busted")
     elif user_score == dealer_score:
-        print("IT is a draw")
+        print("IT is a Draw")
     elif user_score == 0 :
         print("That is BlackJack for the user")
         print("User Wins!!")
@@ -33,6 +36,7 @@ def compare(user_score,dealer_score):
     elif dealer_score > user_score:
         print("Dealer Wins!!")
 def black_jack():
+    """This is the main function which is used to play the game. """
     user_cards = []
     dealer_cards = []
     on = True
@@ -73,7 +77,7 @@ def black_jack():
             else:
                 compare(user_total,dealer_total)
                 on = False
-
+#This is the main loop which is used to restart the game if the user wants to play again.
 restart = "y"
 while restart == "y":
     print("\n"*200)
