dfae605521df426c040545d22a65f7ebc7123f70
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-09 15:46:45 +0530

    Added Comments

--- Day-12-Higher-Lower/main.py
+++ Day-12-Higher-Lower/main.py
@@ -1,16 +1,17 @@
 import random
 import art
 def high_low ():
+    """This Function is a game where the user has to guess a number between 1 and 100. The user can choose the difficulty level, which determines the number of attempts they have to guess the correct number."""
     print(art.logo)
-    number = random.randint(1,100)
+    number = random.randint(1,100) #This is where the system generates a random number between 1 and 100 for the user to guess.
     print("Welcome to the Number Guessing Game")
-    hard_mode = str(input("Choose difficulty: Easy (e) or Hard (h): ")).lower()
-    if hard_mode in ("e" , "easy" ):
+    hard_mode = str(input("Choose difficulty: Easy (e) or Hard (h): ")).lower() #Game mode Selection
+    if hard_mode in ("e" , "easy" ): #Handaling if the user chooses easy instead of e
         life = 10
     else:
         life = 5
     print(f"You now have : {life} life left")
-    while True:
+    while True: #The main loop where the magic happens. The user is prompted to guess a number between 1 and 100, and the system checks if the guess is correct, too high, or too low. The user has a limited number of attempts based on the difficulty level they chose.
         user_guess = int(input("Guess a number between 1 and 100: "))
         if number ==user_guess:
                 print(f"You guessed the number right it was {number}")
@@ -28,7 +29,7 @@ def high_low ():
             break
 
 again = "y"
-while again == "y":
+while again == "y": #This loop allows the user to play the game again if they choose to do so. After each game, the user is prompted to play again or exit the game.
     high_low()
     again = str(input("Do you want to play again? y/n")).lower()
     print("\n" * 200)
