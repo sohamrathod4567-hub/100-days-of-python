f4229d7207a88a3c267753617a31d74148a77a3c
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-14 14:07:52 +0530

    made coffee machine using oop

--- Day-16-Oop/main.py
+++ Day-16-Oop/main.py
@@ -4,7 +4,25 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on =True
 
 
-coffee_maker.report()
-money_machine.report()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like?({options}):  ")
+    if choice == "off":
+        print("Thank you for choosing Us!!")
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    # if choice != options:
+    #     print("please enter a valid choice")
+    #     continue
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+           if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
