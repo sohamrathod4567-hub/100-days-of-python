0eedd733a2e145efbc010b1e3663eee1a247a19f
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-14 15:30:08 +0530

    Added some quiz logic and also loops for it to go to the very end of the question list

--- Day-17-Quiz/main.py
+++ Day-17-Quiz/main.py
@@ -1,11 +1,14 @@
-class User:
-    def __init__(self, user_id, user_name):
-        self.id = user_id
-        self.username = user_name
-        self.followers = 0
-        print("New user being created")
-user1 = User("001" , "soham")
-user2 = User("002" , "nikolas jackson")
-print(user1.id)
-print(user1.username)
-print(user1.followers)
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text , question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
