c06b62ea0ff456d3358dcf7c0d24a87c7cfe272a
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-15 17:05:28 +0530

    Used R G B and Tuple to generate  a random number

--- Day-18-TurtleModule/draw_random_walk.py
+++ Day-18-TurtleModule/draw_random_walk.py
@@ -1,71 +1,21 @@
+import turtle
 from turtle import Turtle,Screen
 import random
-colors = [
-    "red",
-    "blue",
-    "green",
-    "yellow",
-    "orange",
-    "purple",
-    "pink",
-    "brown",
-    "black",
-    "white",
-    "gray",
-    "cyan",
-    "magenta",
-    "gold",
-    "silver",
-    "navy",
-    "teal",
-    "lime",
-    "maroon",
-    "olive",
-    "coral",
-    "turquoise",
-    "violet",
-    "indigo",
-    "khaki",
-    "salmon",
-    "plum",
-    "orchid",
-    "tan",
-    "beige",
-    "chocolate",
-    "crimson",
-    "darkgreen",
-    "darkblue",
-    "darkred",
-    "darkorange",
-    "darkviolet",
-    "deepskyblue",
-    "dodgerblue",
-    "firebrick",
-    "forestgreen",
-    "hotpink",
-    "lavender",
-    "lightblue",
-    "lightgreen",
-    "mediumorchid",
-    "midnightblue",
-    "springgreen",
-    "tomato",
-    "wheat"
-]
+def random_color():
+    r = random.randint (0,255)
+    g = random.randint (0,255)
+    b = random.randint (0,255)
+    color = ( r, g, b )
+    return color
 angles = [0, 45, 90, 135, 180, 225, 270, 315]
 tim = Turtle()
 screen = Screen()
 tim.pensize(15)
-
-def random_walk(disha,pen_color):
-
-        tim.color(pen_color)
-        tim.setheading(disha)
-        tim.forward(50)
-
-while True:
-    turn = random.choice(angles)
-    c = random.choice(colors)
-    random_walk( turn, c )
+turtle.colormode(255)
+screen.bgcolor("black")
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(50)
+    tim.setheading(random.choice(angles))
 
 screen.exitonclick()
