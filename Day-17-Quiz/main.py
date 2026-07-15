41073dced9f6aa4cc70a806fd976460b3e20ed3d
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-15 14:01:40 +0530

    Changed Main file according to the Data file

--- Day-17-Quiz/main.py
+++ Day-17-Quiz/main.py
@@ -4,8 +4,8 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text , question_answer)
     question_bank.append(new_question)
 
@@ -13,3 +13,6 @@ quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
 
+if quiz.still_has_questions() is False:
+    print("You have completed the Quiz")
+    print(f"You Final Score is : {quiz.score} / {len(question_bank)}")
