d9468feb457d254981e35354ab33712e2ce61e84
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-15 15:02:56 +0530

    Made a Square using the turtle modulee

---
+++ Day-18-TurtleModule/main.py
@@ -0,0 +1,25 @@
+from turtle import Turtle,Screen
+
+timmy_the_turtle = Turtle()
+
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.color("blue")
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.color("green")
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+timmy_the_turtle.color("yellow")
+timmy_the_turtle.right(90)
+timmy_the_turtle.forward(100)
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
