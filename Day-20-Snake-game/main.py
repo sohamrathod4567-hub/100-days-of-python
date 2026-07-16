78eed2b63d5ea8746b0e0098b64e8069c51ff237
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-16 17:11:52 +0530

    Used for loop and tuple to make snake instead of the hard coding

--- Day-20-Snake-game/main.py
+++ Day-20-Snake-game/main.py
@@ -4,19 +4,13 @@ screen =Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake-Game")
-
+starting_positions = [(0,0) , (-20,0) , (-40,0)]
 #Here Are 3 Squares for the starting snake
+for position in starting_positions:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
 
-segment_1 = Turtle("square")
-segment_1.color("white")
-
-segment_2 = Turtle("square")
-segment_2.goto( -20,0)
-
-segment_2.color("white")
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(20, 0)
 
 
 
