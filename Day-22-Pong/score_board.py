b3e2f4e0831241d04dbdc29661ab118e3f99f9b9
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-18 18:02:09 +0530

    got some logic fixes

--- Day-22-Pong/score_board.py
+++ Day-22-Pong/score_board.py
@@ -9,5 +9,16 @@ class ScoreBoard(Turtle):
         self.hideturtle()
         self.l_score = 0
         self.r_score = 0
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
         self.goto(x = -10 , y = 250)
         self.write(f" {self.l_score} | {self.r_score}", align="center" , font=("Courier", 24, "normal"))
+
+    def l_point(self):
+        self.l_score +=1
+        self.update_scoreboard()
+    def r_point(self):
+        self.r_score +=1
+        self.update_scoreboard()
