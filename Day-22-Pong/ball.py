d322a31b8aaaa176f9f5bbec2a8882e8f9f0a3c0
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-18 17:22:17 +0530

    logic for the collision with the upper and lower walls

--- Day-22-Pong/ball.py
+++ Day-22-Pong/ball.py
@@ -7,7 +7,11 @@ class Ball(Turtle):
         self.shape("circle")
         self.penup()
         self.color("white")
+        self.x_move = 10
+        self.y_move = 10
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
         self.goto(new_x,new_y)
+    def bounce(self):
+        self.y_move *= -1
