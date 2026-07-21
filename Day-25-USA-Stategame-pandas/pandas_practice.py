83150314427289e3cf7f6e70b8198da7b711f8c1
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-21 14:32:27 +0530

    edited file for practice of pandas

--- Day-25-USA-Stategame-pandas/pandas_practice.py
+++ Day-25-USA-Stategame-pandas/pandas_practice.py
@@ -1,18 +1,35 @@
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(type(data))
 # print(data)
 
-data_dict = data.to_dict()
-print(data_dict)
+# data_dict = data.to_dict()
+# print(data_dict)
+#
+# temp_list = data["temp"].tolist()
+# print(temp_list)
+#
+# print(data["temp"].mean())
+# print(data["temp"].max())
+#
+# #Get the data in columns
+# # # print(data["condition"])
+# # print(data.condition)
+#
+# #Get data in Row
+# print(data[data.day == "Monday"])
+#
+# print(data[data.temp == max(data.temp)])
+#
+# monday = data[data.day == "Monday"]
+# print(monday.temp)
 
-temp_list = data["temp"].tolist()
-print(temp_list)
-
-print(data["temp"].mean())
-print(data["temp"].max())
-
-#Get the data in columns
-# print(data["condition"])
-print(data.condition)
+#Create a dataframe from scratch
+data_dict = {
+    "students": ["Amy", " James", "Angela"],
+    "scores" : [76, 56, 65]
+}
+data = pandas.DataFrame(data_dict)
+print(data)
+data.to_csv("new_data.csv")
