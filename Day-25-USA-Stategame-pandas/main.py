666cec4e1e09a20fec44de5268a1ad80e4080430
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-21 15:54:30 +0530

    Got the hold of the all the states also used list method to convert it all to a list

--- Day-25-USA-Stategame-pandas/main.py
+++ Day-25-USA-Stategame-pandas/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U. S. States Game")
@@ -9,10 +10,18 @@ turtle.shape(image)
 
 # def get_mouse_click_coor(x , y ):
 #     print(x, y)
-# 
+#
 # turtle.onscreenclick(get_mouse_click_coor)
 # turtle.mainloop()
 #
+answer_state = screen.textinput(title="Guess the State", prompt="What is another State's name?").lower()
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+if  answer_state in all_states:
+    print("Correct!")
+
 
 
 
