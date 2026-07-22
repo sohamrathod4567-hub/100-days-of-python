0b5c00bbc7682cf27ad8bc6110a8a8122e21025d
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-22 15:37:18 +0530

    DONE

--- Day-26-Nato-Alphabet/main.py
+++ Day-26-Nato-Alphabet/main.py
@@ -1,5 +1,6 @@
+import pandas
 student_dict = {
-    "student": ["Angela", "James", "Lily"], 
+    "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
@@ -8,7 +9,6 @@ for (key, value) in student_dict.items():
     #Access key and value
     pass
 
-import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
@@ -20,8 +20,15 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+user_input = input("What is your name?").strip().upper()
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+nato = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+nato_pairs = {row.letter:row.code for (index, row) in nato.iterrows()}
+
+print(nato_pairs)
+
+phonetics = [nato_pairs[name] for name in user_input]
+print(phonetics)
+
 
