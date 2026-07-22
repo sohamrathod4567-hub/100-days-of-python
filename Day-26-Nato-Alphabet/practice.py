5d7f356664874ede55f8dfa58a6983acb50c5e77
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-22 13:16:11 +0530

    did some warmup

--- Day-26-Nato-Alphabet/practice.py
+++ Day-26-Nato-Alphabet/practice.py
@@ -1,3 +1,15 @@
 name = "soham"
 new_list = [letter for letter in name] # this is how you do the list comprehension
 print(new_list)
+
+new_numbers = [n*2 for n in range(1,5)]
+print(new_numbers)
+print(new_numbers)
+
+names = ["Alex", "fred","caroline"," simon", "wilson", "lo sinto"]
+
+short_names = [name for name in names if len(name) < 5 ]
+print(short_names)
+
+Cap_names = [name.upper() for name in names if len(name) > 5 ]
+print(Cap_names)
