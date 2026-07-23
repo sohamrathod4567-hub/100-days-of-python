31bd3aae88d1d1e34ac66417f8ea8e74eba0c600
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-23 18:02:37 +0530

    initial commit of Mile to Km converter

---
+++ Day-27-Tkinter-Unit_converter/converter.py
@@ -0,0 +1,13 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Mile to Km Converter")
+
+#Label
+first_label = tkinter.Label(text="is equal to")
+first_label.grid(column=1, row=0)
+
+
+
+
+window.mainloop()
