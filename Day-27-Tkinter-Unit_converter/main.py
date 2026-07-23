e58e773cfe96acd3feed911f67467d1ae7a5946b
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-23 15:43:21 +0530

    More about Tkinter

--- Day-27-Tkinter-Unit_converter/main.py
+++ Day-27-Tkinter-Unit_converter/main.py
@@ -1,10 +1,27 @@
 import tkinter
-
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 #Label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 25))
-my_label.pack(expand=True)
+my_label.pack()
+
+#Button
+
+def button_clicked():
+    my_label["text"]=input.get()
+button = tkinter.Button(text="Hola", command=button_clicked)
+button.pack()
+
+#Entry
+input = tkinter.Entry(width=10)
+input.pack()
+input.get()
+
+
+
+
+
+
 window.mainloop()
