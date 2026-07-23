553ccf8b9a57b4b5583c179e523a159a1654faad
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-23 15:21:11 +0530

    more Practice of **kwargs

--- Day-27-Tkinter-Unit_converter/playground.py
+++ Day-27-Tkinter-Unit_converter/playground.py
@@ -8,8 +8,12 @@ print(add(3,4,5))
 
 def calculate(n , **kwargs):
     print(kwargs)
-    for key , values in kwargs.items():
-        print(key, values)
+    # for key , values in kwargs.items():
+    #     print(key, values)
+    n+= kwargs["add"]
+    n*=kwargs["multiply"]
+    print(n)
+
 
 
 
