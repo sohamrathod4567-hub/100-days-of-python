82cdf618485873c6d07bed4404736b8c88cbee96
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-23 18:30:56 +0530

    Made the fully working miles to Km converter

--- Day-27-Tkinter-Unit_converter/converter.py
+++ Day-27-Tkinter-Unit_converter/converter.py
@@ -1,5 +1,9 @@
 import tkinter
 
+def convert():
+    m = float(mile.get())
+    km["text"] = m *1.6
+
 window = tkinter.Tk()
 window.title("Mile to Km Converter")
 window.geometry("400x300")
@@ -10,7 +14,7 @@ first_label = tkinter.Label(text="is equal to")
 first_label.grid(column=0, row=1)
 
 #input
-mile = tkinter.Entry(width=10)
+mile = (tkinter.Entry(width=10))
 mile.get()
 mile.grid(column=1, row=0)
 
@@ -19,7 +23,8 @@ miles = tkinter.Label(text="Miles")
 miles.grid(column=2, row=0)
 
 #converter
-km = tkinter.Label(text="Km")
+km = tkinter.Label(text="0")
+km.grid(column=1, row=1)
 
 
 #Km
@@ -27,7 +32,7 @@ kilo = tkinter.Label(text="Km")
 kilo.grid(column=2, row=1)
 
 #calculate
-calc = tkinter.Button(text="Calculate")
+calc = tkinter.Button(text="Calculate",command=convert)
 calc.grid(column=1, row=2)
 
 
