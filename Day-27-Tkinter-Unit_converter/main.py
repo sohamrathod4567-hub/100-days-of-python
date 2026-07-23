d33a19a438b60d77df7943627e9909e1add5b97d
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-23 17:59:27 +0530

    Added padding

--- Day-27-Tkinter-Unit_converter/main.py
+++ Day-27-Tkinter-Unit_converter/main.py
@@ -1,23 +1,25 @@
 import tkinter
+def button_clicked():
+    my_label["text"]=input.get()
 window = tkinter.Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 #Label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 25))
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 #Button
-
-def button_clicked():
-    my_label["text"]=input.get()
+new_button = tkinter.Button(text="No piki pani")
+new_button.grid(column=2, row=0)
 button = tkinter.Button(text="Hola", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
 #Entry
 input = tkinter.Entry(width=10)
-input.pack()
 input.get()
+input.grid(column=4, row=4)
 
 
 
