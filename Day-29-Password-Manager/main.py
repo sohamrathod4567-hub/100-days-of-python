a4e60ea75411d8aed4388622e9d325bea15cab5a
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-25 16:12:29 +0530

    Final commit

--- Day-29-Password-Manager/main.py
+++ Day-29-Password-Manager/main.py
@@ -3,25 +3,28 @@ from tkinter import messagebox
 import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 #Password Generator Project
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generator_password():
+    password_entry.delete(0, END)
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-password_letters = [random.choice(letters) for _ in range(nr_letters)]
-password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
-password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
 
-password_list = password_letters + password_symbols + password_numbers
+    password_list = password_letters + password_symbols + password_numbers
 
-random.shuffle(password_list)
+    random.shuffle(password_list)
 
-password  = "".join(password_list)
+    password  = "".join(password_list)
+    password_entry.insert(0,password)
 
-print(f"Your password is: {password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -29,7 +32,7 @@ def save():
     uname = uname_entry.get()
     password_e = password_entry.get()
 
-    if len(website) == 0 or len(password) == 0:
+    if len(website) == 0 or len(password_e) == 0:
         messagebox.showwarning(title="Warning",message="Please enter all details ")
 
     else:
@@ -78,13 +81,13 @@ uname_entry = Entry(width=35)
 uname_entry.grid(row=2, column=1,columnspan=2,sticky="w")
 uname_entry.insert(0,"rathodsoham999@gmail.com")
 
-password_entry = Entry(width=21)
+password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1,sticky="w")
 
 # Buttons
 
 
-generate_password = Button(text="Generate Password")
+generate_password = Button(text="Generate Password" , command=generator_password)
 generate_password.grid(row=3, column=2,sticky="w")
 
 add = Button(text="Add",width=36 , command=save)
