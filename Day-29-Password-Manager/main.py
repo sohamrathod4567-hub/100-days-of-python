f8a1cfa6724aaa5dace42985ed5b0d7b6bf39457
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-25 16:07:45 +0530

    Added the password generator

--- Day-29-Password-Manager/main.py
+++ Day-29-Password-Manager/main.py
@@ -1,21 +1,42 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+#Password Generator Project
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_letters = [random.choice(letters) for _ in range(nr_letters)]
+password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+password_list = password_letters + password_symbols + password_numbers
+
+random.shuffle(password_list)
+
+password  = "".join(password_list)
+
+print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
     uname = uname_entry.get()
-    password = password_entry.get()
+    password_e = password_entry.get()
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning(title="Warning",message="Please enter all details ")
 
     else:
-        is_ok = messagebox.askokcancel(title=website,message=f"These are the details entered :\n Email: {uname} \n Password :  {password}\n is it okay to save")
+        is_ok = messagebox.askokcancel(title=website,message=f"These are the details entered :\n Email: {uname} \n Password :  {password_e}\n is it okay to save")
         if is_ok:
             f = open("data.txt", "a")
-            f.write(f"\n {website} | {uname} | {password}")
+            f.write(f"\n {website} | {uname} | {password_e}")
             f.close()
 
             website_entry.delete(0, END)
