34a366c449cc58954724972f2c843ba00044fee3
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-26 15:36:11 +0530

    Raised my own error

--- Day-30-Errors-Execptions/practice.py
+++ Day-30-Errors-Execptions/practice.py
@@ -1,18 +1,26 @@
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key":"value"}
-    print(a_dictionary["key"])
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key":"value"}
+#     print(a_dictionary["key"])
+#
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed")
+from numpy.random.mtrand import weibull
 
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed")
+height = float(input("Enter your height : "))
+weight = int(input("Enter your weight : "))
 
+
+if height > 3 :
+    raise ValueError("Your height is too high")
+bmi = weight/ height ** 2
 
