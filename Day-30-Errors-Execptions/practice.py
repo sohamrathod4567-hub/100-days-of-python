8324d3f78877e8c3ac2edc33f7da1556954beef3
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-26 15:18:23 +0530

    keyError

--- Day-30-Errors-Execptions/practice.py
+++ Day-30-Errors-Execptions/practice.py
@@ -1,5 +1,12 @@
 try:
     file = open("a_file.txt")
+    a_dictionary = {"key":"value"}
+    print(a_dictionary["key"])
 
 except FileNotFoundError:
     file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
+
+
