dbc15eabfcf360791adfff2e17e8fce89e1d80f1
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-26 15:19:53 +0530

    Else and finally Block

--- Day-30-Errors-Execptions/practice.py
+++ Day-30-Errors-Execptions/practice.py
@@ -8,5 +8,11 @@ except FileNotFoundError:
     file.write("Something")
 except KeyError as error_message:
     print(f"The key {error_message} does not exist")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed")
 
 
