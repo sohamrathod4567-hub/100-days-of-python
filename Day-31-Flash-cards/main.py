7a710827caf4d70a625674136f2dad31ded2e50f
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-27 16:02:49 +0530

    Added the feature to randomly show French words

--- Day-31-Flash-cards/main.py
+++ Day-31-Flash-cards/main.py
@@ -1,30 +1,44 @@
 from tkinter import *
+import pandas
+import random
 
-from pandas.core.interchange import column
-from wcwidth import width
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+data = pandas.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
+
+def next_card():
+    current_card = random.choice(to_learn)
+    card_canvas.itemconfig(card_title, text = "French")
+    card_canvas.itemconfig(card_word, text = current_card["French"])
+
+
+
+
 
 #Set up the screen
 screen = Tk()
 screen.title("Flashy")
-screen.configure(padx=50 , pady=50 ,  bg=BACKGROUND_COLOR)
+screen.configure(padx=50 , pady=50 , bg=BACKGROUND_COLOR)
 
 #set up the initial canvas
 card_canvas = Canvas(width=800 , height=526,background=BACKGROUND_COLOR,highlightthickness=0)
 front_image = PhotoImage(file="images/card_front.png")
 card_canvas.create_image(400 , 263 ,image=front_image)
-card_canvas.create_text(400 , 150,text="Title" , font=("Ariel",40,"italic"))
-card_canvas.create_text(400 , 263,text="word" , font=("Ariel",60,"bold"))
+card_title = card_canvas.create_text(400 , 150,text="Title" , font=("Ariel",40,"italic"))
+card_word = card_canvas.create_text(400 , 263,text="word" , font=("Ariel",60,"bold"))
 
 card_canvas.grid(row=0, column=0,columnspan=2)
 
 cross_image = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=cross_image )
+unknown_button = Button(image=cross_image,command= next_card )
 unknown_button.grid(row=1, column=0)
 
 right_image = PhotoImage(file="images/right.png")
-known_button = Button(image=right_image)
+known_button = Button(image=right_image , command= next_card )
 known_button.grid(row=1, column=1)
 
+next_card()
 screen.mainloop()
