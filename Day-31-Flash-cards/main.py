a66d89a160b14a563dc5f9536f647e1b10ed4292
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-27 16:23:16 +0530

    Added card flip

--- Day-31-Flash-cards/main.py
+++ Day-31-Flash-cards/main.py
@@ -8,13 +8,24 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card ={}
 
 def next_card():
+    global current_card , flip_timer
+    screen.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    card_canvas.itemconfig(card_title, text = "French")
-    card_canvas.itemconfig(card_word, text = current_card["French"])
+    card_canvas.itemconfig(card_title, text = "French" , fill = "black")
+    card_canvas.itemconfig(card_word, text = current_card["French"] , fill = "black")
+    card_canvas.itemconfig(canvas_img, image = front_image)
+    flip_timer= screen.after(3000 , func=translation)
 
 
+
+def translation():
+
+    card_canvas.itemconfig(card_title, text = "English",fill="white")
+    card_canvas.itemconfig(canvas_img, image = back_image)
+    card_canvas.itemconfig(card_word, text = current_card["English"],fill ="white")
 
 
 
@@ -23,10 +34,14 @@ screen = Tk()
 screen.title("Flashy")
 screen.configure(padx=50 , pady=50 , bg=BACKGROUND_COLOR)
 
+flip_timer = screen.after(3000,func=translation)
 #set up the initial canvas
 card_canvas = Canvas(width=800 , height=526,background=BACKGROUND_COLOR,highlightthickness=0)
+
 front_image = PhotoImage(file="images/card_front.png")
-card_canvas.create_image(400 , 263 ,image=front_image)
+back_image = PhotoImage(file="images/card_back.png")
+
+canvas_img = card_canvas.create_image(400 , 263 ,image=front_image)
 card_title = card_canvas.create_text(400 , 150,text="Title" , font=("Ariel",40,"italic"))
 card_word = card_canvas.create_text(400 , 263,text="word" , font=("Ariel",60,"bold"))
 
