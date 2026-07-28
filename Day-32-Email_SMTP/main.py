2b1f28b2607e3abb1cd983dcbec6f4b1ca276216
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-28 20:06:29 +0530

    Added functionality so that the birthday person can be selected

--- Day-32-Email_SMTP/main.py
+++ Day-32-Email_SMTP/main.py
@@ -1,22 +1,28 @@
-##################### Hard Starting Project ######################
+import datetime as dt
+import smtplib
+import random
+import pandas as pd
+TODAY = (7,28)
+MY_EMAIL = "db9695733@gmail.com"
+MY_PASSWORD = "zxlkdltjlglbsjij"
 
-# 1. Update the birthdays.csv with your friends & family's details. 
-# HINT: Make sure one of the entries matches today's date for testing purposes. 
+today = dt.datetime.today()
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Only the month and day matter. 
-# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
-# birthdays_dict = {
-#     (month, day): data_row
-# }
-#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
+data = pd.read_csv("birthdays.csv")
 
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+birthdays_dict = {(data_row.month , data_row.day ):data_row for (index , data_row ) in data.iterrows()}
 
-# 4. Send the letter generated in step 3 to that person's email address.
-# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+if TODAY in birthdays_dict:
+    birthday_person = birthdays_dict[TODAY]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file :
+        contents = letter_file.read()
+        contents.replace("[NAME]",birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com",465) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL , MY_PASSWORD)
+
 
 
 
