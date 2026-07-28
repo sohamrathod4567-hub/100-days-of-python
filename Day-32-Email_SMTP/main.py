37395aadea61512c962086be6dd8e5280f70b4b3
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-28 17:53:53 +0530

    Better way to send an email

--- Day-32-Email_SMTP/main.py
+++ Day-32-Email_SMTP/main.py
@@ -3,11 +3,10 @@ import smtplib
 my_email = "db9695733@gmail.com"
 password = "zxlkdltjlglbsjij"
 
-connection = smtplib.SMTP('smtp.gmail.com', 587)
-connection.starttls()
-connection.login(user=my_email, password= password)
-connection.sendmail(from_addr=my_email, to_addrs="rathodsoham999@gmail.com", msg="ello")
-connection.close()
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password= password)
+    connection.sendmail(from_addr=my_email, to_addrs="rathodsoham999@gmail.com", msg="Subject: popli\n\nello bonjour")
 
 
 
