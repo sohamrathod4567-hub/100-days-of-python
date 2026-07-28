7bd6fd74e9a5be68f940c8815c0e450eccc9376e
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-28 19:19:23 +0530

    Testing some

--- Day-32-Email_SMTP/main.py
+++ Day-32-Email_SMTP/main.py
@@ -1,20 +1,25 @@
 import smtplib
-
+import datetime as dt
+import random
 my_email = "db9695733@gmail.com"
 password = "zxlkdltjlglbsjij"
 
-with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password= password)
-    connection.sendmail(from_addr=my_email, to_addrs="rathodsoham999@gmail.com", msg="Subject: popli\n\nello bonjour")
+with open("quotes.txt", "r") as file :
+    data = file.readlines()
 
+one_quote = random.choice(data)
 
+def send_mail():
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password= password)
+        connection.sendmail(from_addr=my_email, to_addrs="rathodsoham999@gmail.com", msg=f"Subject: Monday Motivation\n\n{one_quote} ")
 
+weekday = dt.datetime.today().weekday()
+print(weekday)
 
-
-
-
-
-
-
+if weekday == 1 :
+    send_mail()
+# date_of_birth = dt.datetime(year=2004 , month=  12 , day=18 )
+# print(date_of_birth)
 
