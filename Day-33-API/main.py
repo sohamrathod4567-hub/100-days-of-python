aa1d46cd4027c1b674058cc11a9243d0c967649e
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-29 16:54:39 +0530

    Got the longitude and latitude of the iss

--- Day-33-API/main.py
+++ Day-33-API/main.py
@@ -2,6 +2,16 @@ import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response)
-if response.status_code != 200:
-    print("Something went wrong.")
-    raise Exception("Bad response from iss API")
+# if response.status_code != 200:
+#     print("Something went wrong.")
+#     raise Exception("Bad response from iss API")
+response.raise_for_status()
+
+data = response.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (latitude, longitude)
+
+print(iss_position)
