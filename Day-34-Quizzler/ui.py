66e8713f4f7410fb148cbb7efc1087ee85630c11
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-31 14:32:57 +0530

    Made some more changes in the Ui

--- Day-34-Quizzler/ui.py
+++ Day-34-Quizzler/ui.py
@@ -1,16 +1,22 @@
 from tkinter import *
 THEME_COLOR = "#375362"
+FONT = ("Arial" , 20 , "italic")
 
-class QuizInterface():
+class QuizInterface:
 
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
+        self.text = Label(text="Score : 0",bg=THEME_COLOR,fg="white" , font=("Arial" , 10 , "bold"))
+        self.text.grid(row=0, column=1)
+
+
+
         self.canvas = Canvas(width=250, height=300, bg="white")
-        self.canvas.create_text( 125 ,  150, text = "Pablo" , font= ("Arial" , 20 , "italic"))
-        self.canvas.grid(row=0, column=0)
+        self.canvas.create_text( 125 ,  150, text = "Pablo" , font= FONT)
+        self.canvas.grid(row=1, column=0,columnspan=2)
 
 
         self.window.mainloop()
