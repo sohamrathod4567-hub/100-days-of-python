a8a3fc3f95fd612fa9bcdda6986f1678442dfceb
Soham Rathod <sohamrathod4567@gmail.com>
2026-07-31 14:24:29 +0530

    Made initial User InterFace

--- Day-34-Quizzler/ui.py
+++ Day-34-Quizzler/ui.py
@@ -6,6 +6,11 @@ class QuizInterface():
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.canvas = Canvas(width=250, height=300, bg="white")
+        self.canvas.create_text( 125 ,  150, text = "Pablo" , font= ("Arial" , 20 , "italic"))
+        self.canvas.grid(row=0, column=0)
 
 
         self.window.mainloop()
