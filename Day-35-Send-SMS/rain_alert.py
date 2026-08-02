7cd30a793e2f268345370474cd7fdf3229598c72
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-02 17:25:19 +0530

    Cleaned

--- Day-35-Send-SMS/rain_alert.py
+++ Day-35-Send-SMS/rain_alert.py
@@ -1,5 +1,8 @@
 import requests
-api_key = ""
+from twilio.rest import Client
+api_key = "API key here"
+account_sid = "Account Sid here"
+auth_token = "Auth token here"
 
 parameters = {
     "key":api_key,
@@ -12,8 +15,17 @@ response = requests.get("http://api.weatherapi.com/v1/current.json", params=para
 response.raise_for_status()
 response_json = response.json()
 will_it_rain = response_json["current"]["chance_of_rain"]
+print(will_it_rain)
 
 if will_it_rain > 30:
+    client = Client(account_sid , auth_token)
+    message = client.messages \
+        .create(
+        body= " It's Going to Rain Nigah",
+        from= "The Twilio Number",
+        to = "Your-number"
+
+    )
     print("Bring the Umbrella nigah")
 else:
     print("It's alright Nigah")
