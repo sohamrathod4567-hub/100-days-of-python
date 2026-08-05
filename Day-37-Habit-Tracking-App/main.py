ea12d50476b58583c9edf37b72047b462ad7317e
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-05 16:13:04 +0530

    Added 1 pixel

--- Day-37-Habit-Tracking-App/main.py
+++ Day-37-Habit-Tracking-App/main.py
@@ -1,8 +1,9 @@
 import requests
 
-
+# Link : https://pixe.la/v1/users/soham9/graphs/graph1.html   From here we can access the graph
 USERNAME = "soham9"
 TOKEN = "123456789"
+GRAPH_ID = "graph1"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
@@ -18,7 +19,7 @@ user_params = {
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 graph_config = {
-    "id": "graph1",
+    "id": GRAPH_ID,
     "name": "BookReading",
     "unit": "Pages",
     "type": "float",
@@ -30,3 +31,13 @@ headers = {
 
 # response = requests.post(url=graph_endpoint, json=graph_config , headers=headers)
 # print(response.text)
+
+pixel_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+pixel_config = {
+    "date": "20260805",
+    "quantity":"7"
+}
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+
+print(response.text)
