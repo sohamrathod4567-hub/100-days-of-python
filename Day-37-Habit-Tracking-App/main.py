6655e3c931d841922af367f95ebb7d4357f7fa65
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-05 16:05:47 +0530

    Created An empty graph in the pixela

--- Day-37-Habit-Tracking-App/main.py
+++ Day-37-Habit-Tracking-App/main.py
@@ -1,13 +1,32 @@
 import requests
 
+
+USERNAME = "soham9"
+TOKEN = "123456789"
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "123456789",
-    "username": "soham9",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor" : "yes"
 
 }
 # response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "BookReading",
+    "unit": "Pages",
+    "type": "float",
+    "color": "ajisai"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config , headers=headers)
 # print(response.text)
