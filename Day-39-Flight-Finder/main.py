80408e675d594ab0714926f61b6bcfa5a6f4ae1a
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-06 16:43:46 +0530

    Got the hold of tomorrow's date and also six month late's date (used timedelta ( ) )

--- Day-39-Flight-Finder/main.py
+++ Day-39-Flight-Finder/main.py
@@ -2,6 +2,10 @@ import requests_cache
 import os
 from pprint import pprint
 from dotenv import load_dotenv
+from  datetime import datetime,timedelta
+
+tomorrow = datetime.today() + timedelta(days=1)
+six_month_from_today = datetime.today() + timedelta(days=182)
 
 load_dotenv()
 requests_cache.install_cache()
