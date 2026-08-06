258a1ab3ec89820daacddda88f646691fba5cea9
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-06 17:41:30 +0530

    Made some changes

--- Day-39-Flight-Finder/data_manager.py
+++ Day-39-Flight-Finder/data_manager.py
@@ -1,13 +1,22 @@
 import requests
 import os
 from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+
 load_dotenv()
 SHEETY_API_ENDPOINT = os.getenv("SHEETY_API")
 USERNAME = os.getenv("USER_NAME")
 PASSWORD = os.getenv("PASSWORD")
+AUTHORIZATION = HTTPBasicAuth(USERNAME, PASSWORD)
 
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
     def __init__(self):
-        self.response = requests.get(SHEETY_API_ENDPOINT,auth=(USERNAME, PASSWORD))
-        self.prices = self.response.json()
+
+        self.destination_data = {}
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_API_ENDPOINT, auth=AUTHORIZATION)
+        response.raise_for_status()
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
