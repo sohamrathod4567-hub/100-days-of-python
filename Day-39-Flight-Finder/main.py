8c05ca7f15400a39f17222a4caf489f923315417
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-06 16:25:39 +0530

    Got the data in a formated way

--- Day-39-Flight-Finder/main.py
+++ Day-39-Flight-Finder/main.py
@@ -1,1 +1,11 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+import os
+from pprint import pprint
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from data_manager import DataManager
+SERP_API = os.getenv("SERP_API")
+
+sheet_data = DataManager()
+pprint(sheet_data.prices)
