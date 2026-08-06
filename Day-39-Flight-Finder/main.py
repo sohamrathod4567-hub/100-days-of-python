4eb0323f85351035c3acdf2fe57a9d25c266ea3d
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-06 17:04:01 +0530

    Called my first Flight api

--- Day-39-Flight-Finder/main.py
+++ Day-39-Flight-Finder/main.py
@@ -3,6 +3,7 @@ import os
 from pprint import pprint
 from dotenv import load_dotenv
 from  datetime import datetime,timedelta
+from flight_search import FlightSearch
 
 tomorrow = datetime.today().date() + timedelta(days=1)
 six_month_from_today = datetime.today().date() + timedelta(days=182)
@@ -14,4 +15,7 @@ from data_manager import DataManager
 SERP_API = os.getenv("SERP_API")
 
 sheet_data = DataManager()
-pprint(sheet_data.prices)
+# pprint(sheet_data.prices)
+
+search = FlightSearch().check_flights()
+pprint(search)
