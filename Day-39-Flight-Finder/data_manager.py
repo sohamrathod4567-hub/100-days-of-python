b140342bb562ff5edb1d5b6e4a7c4319e353a8f3
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-06 16:25:12 +0530

    Added the sheety api to get hold of the sheets

--- Day-39-Flight-Finder/data_manager.py
+++ Day-39-Flight-Finder/data_manager.py
@@ -1,3 +1,13 @@
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+SHEETY_API_ENDPOINT = os.getenv("SHEETY_API")
+USERNAME = os.getenv("USER_NAME")
+PASSWORD = os.getenv("PASSWORD")
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self):
+        self.response = requests.get(SHEETY_API_ENDPOINT,auth=(USERNAME, PASSWORD))
+        self.prices = self.response.json()
