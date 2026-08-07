445a84f216382bcb148a94d7856175af13c56b0b
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-07 16:13:20 +0530

    Now there will be data on the cheapest flight in this

--- Day-39-Flight-Finder/flight_data.py
+++ Day-39-Flight-Finder/flight_data.py
@@ -6,3 +6,39 @@ class FlightData:
         self.destination_airport = destination_airport
         self.out_date = out_date
         self.return_date = return_date
+
+    def find_cheapest_flight(self, data , return_date):
+        #Handle empty data if no flight data is returned
+        if data is None or (not data.get("best_flights") and not data.get("other_flights")):
+            print("No Flight Data")
+            return FlightData("N/A","N/A","N/A","N/A","N/A")
+
+        # Combine Best_flights and other_flights
+        all_flights = data.get("best_flights", []) + data.get("other_flights",[])
+
+        # Data from the first flight in the list
+        first_flight = all_flights[0]
+        lowest_price = first_flight["price"]
+        origin = first_flight["flights"][0]["departure_airport"]["id"]
+        destination = first_flight["flights"][-1]["arrival_airport"]["id"]
+        out_date = first_flight["flights"][0]["departure_airport"]["time"].split(" ")[0]
+
+        # Initialize FlightData with the first flight for comparison
+
+        cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
+
+        for flight in all_flights:
+            try :
+                price = flight["price"]
+            except KeyError:
+                price = "N/A"
+                continue
+            if price < lowest_price:
+                lowest_price = price
+                origin = flight["flights"][0]["departure_airport"]["id"]
+                destination = flight["flights"][-1]["arrival_airport"]["id"]
+                out_date = flight["flights"][0]["departure_airport"]["time"].split(" ")[0]
+                cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
+                print(f"Lowest Price to {destination} is GBP {lowest_price}")
+
+        return cheapest_flight
