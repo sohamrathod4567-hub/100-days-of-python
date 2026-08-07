6ffabc248327f71f0ffedd5fc11bfee9014b16b8
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-07 15:17:21 +0530

    Initial commit for the flight data

--- Day-39-Flight-Finder/flight_data.py
+++ Day-39-Flight-Finder/flight_data.py
@@ -1,3 +1,8 @@
 class FlightData:
     #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self):
+        self.price =
+        self.origin_airport =
+        self.destination_airport =
+        self.out_date =
+        self.return_date =
