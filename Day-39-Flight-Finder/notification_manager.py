d92bff7484ef839a1dfe27483989a4ebb5372a02
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-07 19:25:35 +0530

    Fixed some mail related Bugs

--- Day-39-Flight-Finder/notification_manager.py
+++ Day-39-Flight-Finder/notification_manager.py
@@ -8,11 +8,11 @@ PASSWORD = os.getenv("MY_PASSWORD")
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
    def __init__(self,content):
-
-            with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        body = "\n".join(content)
+        with smtplib.SMTP("smtp.gmail.com", 587) as connection:
                 connection.starttls()
                 connection.login(EMAIL, PASSWORD)
                 connection.sendmail(from_addr=EMAIL,
-                                    to_addrs=rathodsoham999@gmail.com,
-                                    msg=f"{content}"
+                                    to_addrs="rathodsoham999@gmail.com",
+                                    msg=f"Subject:Cheap Flight NotiFication \n \n  {body}"
                                     )
