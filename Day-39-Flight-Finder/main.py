5492a9216a77de6e62080afe43965cc3af0cbf1b
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-07 19:25:12 +0530

    ALmost FInal code

--- Day-39-Flight-Finder/main.py
+++ Day-39-Flight-Finder/main.py
@@ -6,7 +6,7 @@ from  datetime import datetime,timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import find_cheapest_flight
-
+from notification_manager import NotificationManager
 
 tomorrow = datetime.today().date() + timedelta(days=1)
 six_month_from_today = datetime.today().date() + timedelta(days=182)
@@ -21,6 +21,8 @@ destination_data = sheet_data.get_destination_data()
 pprint(destination_data)
 search = FlightSearch()
 
+message = []
+
 ORIGIN_CITY_IATA = "LHR"
 for destination in destination_data:
     pprint(f"Getting Flights for {destination["city"]}...")
@@ -33,8 +35,10 @@ for destination in destination_data:
     pprint(flights)
 
     cheapest_flight = find_cheapest_flight(flights , return_date=six_month_from_today)
-    pprint(f"{destination['city']}: GBP {cheapest_flight.price}")
+    message.append(f"{destination['city']}: GBP {cheapest_flight.price}")
 
     if cheapest_flight.price != "N/A" and cheapest_flight.price < destination['lowestPrice']:
-        pprint(f"Lower Price Flight found to {destination['city']}!!")
+        message.append(f"Lower Price Flight found to {destination['city']}!!")
         sheet_data.update_lowest_price(destination["id"],cheapest_flight.price)
+
+email = NotificationManager(message)
