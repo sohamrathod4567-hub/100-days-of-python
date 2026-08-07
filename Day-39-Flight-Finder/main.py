cbe2b9770e6d9fbb973195299b85536d218f3a7f
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-07 16:38:44 +0530

    Trying to implement the cheapest flight module

--- Day-39-Flight-Finder/main.py
+++ Day-39-Flight-Finder/main.py
@@ -3,7 +3,10 @@ import os
 from pprint import pprint
 from dotenv import load_dotenv
 from  datetime import datetime,timedelta
+from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+
 
 tomorrow = datetime.today().date() + timedelta(days=1)
 six_month_from_today = datetime.today().date() + timedelta(days=182)
@@ -11,11 +14,11 @@ six_month_from_today = datetime.today().date() + timedelta(days=182)
 load_dotenv()
 requests_cache.install_cache()
 
-from data_manager import DataManager
 SERP_API = os.getenv("SERP_API")
 
-sheet_data = DataManager().get_destination_data()
-pprint(sheet_data)
+sheet_data = DataManager()
+destination_data = sheet_data.get_destination_data()
+pprint(destination_data)
 
 search = FlightSearch()
 flights = search.check_flights(
@@ -25,3 +28,10 @@ flights = search.check_flights(
     to_time=six_month_from_today,
 )
 pprint(flights)
+
+cheapest_flight = find_cheapest_flight(flights , return_date=six_month_from_today)
+pprint(f"{destination_data[0]['city']}: GBP {cheapest_flight.price}")
+
+if cheapest_flight.price != "N/A" and cheapest_flight.price < destination_data[0]['lowestprice']:
+    pprint(f"Lower Price Flight found to {destination_data[0]['city']}!!")
+    sheet_data.update_lowest_price(destination_data[0]["id"], cheapest_flight.price)
