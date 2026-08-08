8bf62632a3819449809992283820e2427d5ebb19
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-08 16:17:11 +0530

    Copied from the last day to add new features

---
+++ Day-40-Flight_Club/flight_search.py
@@ -0,0 +1,36 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+class FlightSearch:
+    #This class is responsible for talking to the Flight Search API.
+    def __init__(self):
+        self.response = None
+        self._api_key = os.getenv("SERP_API")
+        self.end_point = "https://serpapi.com/search"
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        query = {
+            "engine": "google_flights",
+            "departure_id": origin_city_code,
+            "arrival_id": destination_city_code,
+            "outbound_date": from_time.strftime("%Y-%m-%d"),
+            "return_date": to_time.strftime("%Y-%m-%d"),
+            "type": "1",
+            "adults": "1",
+            "currency": "GBP",
+            "api_key": self._api_key,
+        }
+        response = requests.get(self.end_point, params=query)
+
+        if response.status_code != 200:
+            print(f"Check_flights() Response code : {response.status_code}")
+            return None
+
+        data = response.json()
+        if "error" in data :
+            print(f"API ERROR : {data['error']}")
+            return None
+        return data
