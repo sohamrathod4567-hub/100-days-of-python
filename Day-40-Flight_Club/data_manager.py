d52e912788d535b952c11912a82f1f3f734bb654
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-08 16:17:00 +0530

    Copied from the last day to add new features

---
+++ Day-40-Flight_Club/data_manager.py
@@ -0,0 +1,34 @@
+import requests
+import os
+from dotenv import load_dotenv
+from requests.auth import HTTPBasicAuth
+
+load_dotenv()
+SHEETY_API_ENDPOINT = os.getenv("SHEETY_API")
+USERNAME = os.getenv("USER_NAME")
+PASSWORD = os.getenv("PASSWORD")
+AUTHORIZATION = HTTPBasicAuth(USERNAME, PASSWORD)
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+    def __init__(self):
+
+        self.destination_data = {}
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_API_ENDPOINT, auth=AUTHORIZATION)
+        response.raise_for_status()
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_lowest_price(self,row_id, new_price):
+        new_data = {
+            "price": {
+                "lowestPrice": new_price
+            }
+        }
+        requests.put(
+            url=f"{SHEETY_API_ENDPOINT}/{row_id}",
+            json=new_data,
+            auth=AUTHORIZATION
+        )
