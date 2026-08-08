fa61560ce68a60a715d7cbae06f3d81ef6d032bc
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-08 16:17:05 +0530

    Copied from the last day to add new features

---
+++ Day-40-Flight_Club/flight_data.py
@@ -0,0 +1,45 @@
+def find_cheapest_flight(data, return_date):
+    #Handle empty data if no flight data is returned
+    if data is None or (not data.get("best_flights") and not data.get("other_flights")):
+        print("No Flight Data")
+        return FlightData("N/A","N/A","N/A","N/A","N/A")
+
+    # Combine Best_flights and other_flights
+    all_flights = data.get("best_flights", []) + data.get("other_flights",[])
+
+    # Data from the first flight in the list
+    first_flight = all_flights[0]
+    lowest_price = first_flight["price"]
+    origin = first_flight["flights"][0]["departure_airport"]["id"]
+    destination = first_flight["flights"][-1]["arrival_airport"]["id"]
+    out_date = first_flight["flights"][0]["departure_airport"]["time"].split(" ")[0]
+
+    # Initialize FlightData with the first flight for comparison
+
+    cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
+
+    for flight in all_flights:
+        try :
+            price = flight["price"]
+        except KeyError:
+            price = "N/A"
+            continue
+        if price < lowest_price:
+            lowest_price = price
+            origin = flight["flights"][0]["departure_airport"]["id"]
+            destination = flight["flights"][-1]["arrival_airport"]["id"]
+            out_date = flight["flights"][0]["departure_airport"]["time"].split(" ")[0]
+            cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
+            print(f"Lowest Price to {destination} is GBP {lowest_price}")
+
+    return cheapest_flight
+
+
+class FlightData:
+    #This class is responsible for structuring the flight data.
+    def __init__(self , price , origin_airport , destination_airport , out_date , return_date):
+        self.price = price
+        self.origin_airport = origin_airport
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
