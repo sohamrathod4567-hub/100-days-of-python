a9650626af8e29f62d6fed5ea3fc954db19f5b43
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-19 16:56:27 +0530

    Accessing using id and class

--- Day45-Beautiful-Soup/main.py
+++ Day45-Beautiful-Soup/main.py
@@ -9,7 +9,13 @@ soup = BeautifulSoup(contents , "html.parser")
 # print(soup.a)
 all_anchor_tags = soup.find_all(name = "a")
 # print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     # print(tag.getText())
+#     print(tag.get("href"))
 
-for tag in all_anchor_tags:
-    # print(tag.getText())
-    print(tag.get("href"))
+heading = soup.find(name = "h1" , id = "name")
+print(heading)
+
+section_heading = soup.find(name="h3", class_=("heading"))
+print(section_heading)
