85e05ffd0c0ddbf62468e0c1d980de1aae06f1e9
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-21 18:33:02 +0530

    Got it to print 100 movies to the console

--- Day45-Beautiful-Soup/Top-100.py
+++ Day45-Beautiful-Soup/Top-100.py
@@ -3,6 +3,17 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
+response = requests.get(URL)
+top_web_page = response.text
 
+soup = BeautifulSoup(top_web_page, "html.parser")
 
+# print(soup.title)
+movie_titles = soup.find_all(name ="h3",class_ ="title")
+movies= []
 
+for title in movie_titles:
+    movies.append(title.text)
+
+movies.reverse()
+print(movies)
