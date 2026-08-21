c7e262ce9b498dea763c9686122cb8d1e5c978e2
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-21 17:37:53 +0530

    Fixed the bug

--- Day45-Beautiful-Soup/other.py
+++ Day45-Beautiful-Soup/other.py
@@ -13,7 +13,7 @@ for article_tag in articles:
 
     article_text = article_tag.getText()
     article_texts.append(article_text)
-    article_link = article_tag.get("href")
+    article_link = article_tag.find("a").get("href")
     article_links.append(article_link)
 
 article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all("span",class_="score")]
