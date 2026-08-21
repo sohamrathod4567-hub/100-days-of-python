416b624286430f7a460173ae919a52abdfc0073c
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-21 17:06:01 +0530

    Got the text from it

--- Day45-Beautiful-Soup/other.py
+++ Day45-Beautiful-Soup/other.py
@@ -6,5 +6,6 @@ yc_web_pages = response.text
 
 soup = BeautifulSoup(yc_web_pages, "html.parser")
 print(soup.title)
-article = soup.find(name="a",class_ ="storyLink")
-print(article)
+article_tag = soup.find(name="span",class_ ="titleline")
+print(article_tag)
+article_text = article_tag.getText()
