fd7e0b6f2606e5163ba11a499eab8b56533453f0
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-22 16:15:38 +0530

    Got song title

--- Day-46-Spotify-Playlist-Musical-Time-Machine/main.py
+++ Day-46-Spotify-Playlist-Musical-Time-Machine/main.py
@@ -6,6 +6,8 @@ URL = f"https://appbrewery.github.io/bakeboard-hot-100/{date}/"
 
 response = requests.get(URL)
 bill_bord_web_page = response.text
-print(bill_bord_web_page)
+# print(bill_bord_web_page)
 
 soup = BeautifulSoup(bill_bord_web_page, "html.parser")
+song_titles=soup.find(name="h3",class_="chart-entry__title")
+print(song_titles)
