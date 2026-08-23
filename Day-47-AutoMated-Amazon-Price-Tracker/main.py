f4ae5e9d77439848707f1f8a0669176c0d12a200
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-23 17:30:00 +0530

    Got the numbers (Price from the website)

--- Day-47-AutoMated-Amazon-Price-Tracker/main.py
+++ Day-47-AutoMated-Amazon-Price-Tracker/main.py
@@ -3,5 +3,9 @@ from bs4 import BeautifulSoup
 URL = "https://appbrewery.github.io/instant_pot/"
 
 response = requests.get(URL)
-soup = BeautifulSoup(response.content, "html.parser")
-print(soup)
+product_web_page = response.text
+soup = BeautifulSoup(product_web_page, "html.parser")
+# print(soup)
+price = soup.find(name="span", class_="a-price-whole").get_text(strip=True)
+price = float(price.replace(".", ""))
+print(price)
