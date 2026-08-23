7ca3ed7cd33b7bbd624fc76fbd5904c745f5d06a
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-23 17:48:42 +0530

    Used The live Amazon Website

--- Day-47-AutoMated-Amazon-Price-Tracker/main.py
+++ Day-47-AutoMated-Amazon-Price-Tracker/main.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 MY_EMAIL = os.getenv("G_USERNAME")
 MY_PASSWORD = os.getenv("G_PASSWORD")
-URL = "https://appbrewery.github.io/instant_pot/"
+URL = "https://www.amazon.com/dp/B01NBKTPTS?th=1"
 TARGET_PRICE = 100
 
 response = requests.get(URL)
@@ -27,3 +27,5 @@ if price < TARGET_PRICE:
                             msg=f"Subject :Price Drop alert!\n\n Hello there , hope you are having a great day, the product which you wanted to buy from a really long time has a price drop , check it out over here {URL}"
                             )
     print("Mail sent!!")
+else:
+    print("The price have not met your condition")
