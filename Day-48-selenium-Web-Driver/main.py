2a528966093edfe728b7e73a4d0acb1464b278f1
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-24 16:45:19 +0530

    This is how you keep your Chrome tab open

--- Day-48-selenium-Web-Driver/main.py
+++ Day-48-selenium-Web-Driver/main.py
@@ -1,4 +1,4 @@
-from pandas.core.indexing import check_dict_or_set_indexers
+
 from selenium import webdriver
 
 # keep the Chrome browser Open After program Finishes
@@ -8,3 +8,6 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.amazon.com")
+
+# driver.close()
+# driver.quit()
