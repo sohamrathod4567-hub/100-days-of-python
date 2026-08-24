2a9a94ad717b4c17702ed5bbdddef573a6463e4b
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-24 17:04:07 +0530

    Did this with python official website

--- Day-48-selenium-Web-Driver/main.py
+++ Day-48-selenium-Web-Driver/main.py
@@ -7,12 +7,13 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://appbrewery.github.io/instant_pot/")
+driver.get("https://www.python.org")
+#
+# price_dolla = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+# price_cent = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+# print(f"The Price is {price_dolla.text}.{price_cent.text}")
 
-price_dolla = driver.find_element(By.CLASS_NAME, value="a-price-whole")
-price_cent = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
-print(f"The Price is {price_dolla.text}.{price_cent.text}")
-
-
+search_bar = driver.find_element(By.NAME, value="q")
+print(search_bar.get_attribute("placeholder"))
 # driver.close()
 driver.quit()
