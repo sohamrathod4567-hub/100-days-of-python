8505b5d200926a4d28cf5bc6a615e1471e16ec46
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-24 17:21:23 +0530

    Got all the items from the events tab

--- Day-48-selenium-Web-Driver/E1.py
+++ Day-48-selenium-Web-Driver/E1.py
@@ -6,3 +6,9 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.python.org")
+
+whole_thing = driver.find_element(By.CLASS_NAME, value="shrubbery")
+
+print(whole_thing.text)
+
+driver.quit()
