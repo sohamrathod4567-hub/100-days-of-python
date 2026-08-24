cb9fa29f8863ee83032d72c98ce5d724c858264e
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-24 17:17:34 +0530

    Initial commit for the exercise1

---
+++ Day-48-selenium-Web-Driver/E1.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.python.org")
