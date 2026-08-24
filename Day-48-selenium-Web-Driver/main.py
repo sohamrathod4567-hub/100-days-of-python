399273aff10ce7188fe5e3fa168ec190d9e09001
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-24 17:00:21 +0530

    Did the last day's project with ease

--- Day-48-selenium-Web-Driver/main.py
+++ Day-48-selenium-Web-Driver/main.py
@@ -1,5 +1,5 @@
-
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 # keep the Chrome browser Open After program Finishes
 
@@ -7,7 +7,12 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com")
+driver.get("https://appbrewery.github.io/instant_pot/")
+
+price_dolla = driver.find_element(By.CLASS_NAME, value="a-price-whole")
+price_cent = driver.find_element(By.CLASS_NAME, value="a-price-fraction")
+print(f"The Price is {price_dolla.text}.{price_cent.text}")
+
 
 # driver.close()
-# driver.quit()
+driver.quit()
