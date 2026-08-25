931a2b0072280c2a1334d621c9ef84e69ae6854c
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-25 16:08:12 +0530

    Got everything set for the bot

--- Day-49-Gym-Automation--Bot/main.py
+++ Day-49-Gym-Automation--Bot/main.py
@@ -6,3 +6,8 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
 
 user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://appbrewery.github.io/gym/")
