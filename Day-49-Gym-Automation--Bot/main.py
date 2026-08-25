14780f1e1df39fdaf4a20b90b0db7779ed8045bc
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-25 16:22:35 +0530

    Logged in automatically

--- Day-49-Gym-Automation--Bot/main.py
+++ Day-49-Gym-Automation--Bot/main.py
@@ -2,9 +2,9 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-ACCOUNT_EMAIL= "sohamrathod999@gmail.com"
-ACCOUNT_PASSWORD = "9879915801"
-GYM_URL = "https://appbrewery.github.io/gym/"
+ACCOUNT_EMAIL= "rathodsoham999@gmail.com"
+ACCOUNT_PASSWORD ="9879915801"
+GYM_URL ="https://appbrewery.github.io/gym/"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
 
@@ -14,3 +14,15 @@ chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get(GYM_URL)
+
+login_button = driver.find_element(By.ID, "login-button")
+login_button.click()
+
+login_email = driver.find_element(By.NAME, "email")
+login_email.send_keys(ACCOUNT_EMAIL)
+
+login_password = driver.find_element(By.NAME, "password")
+login_password.send_keys(ACCOUNT_PASSWORD)
+
+submit_btn = driver.find_element(By.ID, "submit-button")
+submit_btn.click()
