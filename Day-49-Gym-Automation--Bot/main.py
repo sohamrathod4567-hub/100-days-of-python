95f99771d5c1d9cdcc2736fe0cffa68d35685e6a
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-25 16:11:41 +0530

    Got the code running and also made an account on the website

--- Day-49-Gym-Automation--Bot/main.py
+++ Day-49-Gym-Automation--Bot/main.py
@@ -2,6 +2,9 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+ACCOUNT_EMAIL= "sohamrathod999@gmail.com"
+ACCOUNT_PASSWORD = "9879915801"
+GYM_URL = "https://appbrewery.github.io/gym/"
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach",True)
 
@@ -10,4 +13,4 @@ user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
 chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://appbrewery.github.io/gym/")
+driver.get(GYM_URL)
