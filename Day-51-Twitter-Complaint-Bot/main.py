9c671d7cae0161340de7edb467697b2e21e5842e
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-30 16:39:08 +0530

    Added class for ease of usage

--- Day-51-Twitter-Complaint-Bot/main.py
+++ Day-51-Twitter-Complaint-Bot/main.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -33,3 +32,20 @@ password.send_keys(Y_PASSWORD)
 
 sign_in = driver.find_element(By.XPATH, "/html/body/div/div/form/button")
 sign_in.click()
+
+class InternetSpeedTwitterBot:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.up = 0
+        self.down = 0
+
+    def get_internet_speed(self):
+        pass
+
+    def tweet_at_provider(self):
+        pass
+
+
+bot = InternetSpeedTwitterBot()
+bot.get_internet_speed()
+bot.tweet_at_provider()
