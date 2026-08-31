bfd986687d40d749c680c9e9ae803636432a55db
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-31 18:40:12 +0530

    Initial commit for the bot, but I am using the Share-a-Naan application

---
+++ Day-52-Instagram-Follower-Bot/main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+
+EMAIL = "rathodsoham999@gmail.com"
+PASSWORD = "Z0ZINta5FXA38ihg"
+SIMILAR_ACCOUNT = "rordongamsay"
