08d80e3669b70fd17e7cb256377b3244d0f26351
Soham Rathod <sohamrathod4567@gmail.com>
2026-08-31 19:25:14 +0530

    Got inside and entered the Username and Password.

--- Day-52-Instagram-Follower-Bot/main.py
+++ Day-52-Instagram-Follower-Bot/main.py
@@ -8,3 +8,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 EMAIL = "rathodsoham999@gmail.com"
 PASSWORD = "Z0ZINta5FXA38ihg"
 SIMILAR_ACCOUNT = "rordongamsay"
+LOGIN_URL = "https://app.100daysofpython.dev/services/share-a-naan/welcome"
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach",True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(LOGIN_URL)
+
+login_btn = driver.find_element(By.XPATH, "/html/body/div/aside/div/form/button")
+login_btn.click()
+
+username = driver.find_element(By.ID, 'username')
+username.send_keys(EMAIL)
+
+password = driver.find_element(By.ID, 'password')
+password.send_keys(PASSWORD)
