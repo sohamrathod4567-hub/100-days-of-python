14da353fd429935d9f3745d79fcd89a3950473da
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-01 17:05:18 +0530

    Made empty lists and also got all the addy

--- Day-53-Data-Entry-Automation/main.py
+++ Day-53-Data-Entry-Automation/main.py
@@ -9,7 +9,15 @@ FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSc7vMuMtdjXJAqHWnpg37f17tGH
 response = requests.get(URL)
 zillow_web_page = response.text
 
+addy = []
+price = []
+links = []
+
 
 soup = BeautifulSoup(zillow_web_page, "html.parser")
 # print(soup.prettify())
-address = soup.find_all(name = "a",class_= "StyledPropertyCardDataArea-anchor")
+all_addresses = soup.find_all("address", attrs={"data-test": "property-card-addr"})
+print(all_addresses)
+
+for address in all_addresses:
+    print(address.get_text(strip=True))
