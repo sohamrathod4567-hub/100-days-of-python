e8c957bd5c803befded2cedca434e0c9e65d440e
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-02 16:58:19 +0530

    Used a different approach but in the end the thing is working just fine

--- Day-53-Data-Entry-Automation/main.py
+++ Day-53-Data-Entry-Automation/main.py
@@ -1,12 +1,10 @@
-from time import sleep
-
+from time import sleep, time
 import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import re
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
 
 URL = "https://appbrewery.github.io/Zillow-Clone/"
 
@@ -16,7 +14,7 @@ response = requests.get(URL)
 zillow_web_page = response.text
 
 addy = []
-price = []
+prices = []
 links = []
 
 
@@ -41,11 +39,16 @@ for p in all_prices:
     price_text = p.get_text(strip=True)
     price_only = re.search(r"\$[\d,]+", price_text).group()
     # print(price_only)
-    price.append(price_only)
+    prices.append(price_only)
 
 # print(price)
 
 # Got the Links
+
+all_link_elements = soup.select(".StyledPropertyCardDataWrapper a")
+all_links = [link["href"] for link in all_link_elements]
+print(f"There are {len(all_links)} links to individual listings in total: \n")
+print(all_links)
 
 all_links = soup.find_all("a", attrs={"data-test": "property-card-link"})
 
@@ -53,57 +56,32 @@ for price_per_month in all_links:
     links.append(price_per_month.get("href"))
 # print(links)
 
-# # This keeps our browser open
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_experimental_option("detach",True)
-#
-#
-#
-# #The Driver
-# driver = webdriver.Chrome(options=chrome_options)
-# driver.get(FORM_URL)
-#
-# wait = WebDriverWait(driver, 2)
-# inputa =(By.CSS_SELECTOR, "input.whsOnd")
-# add = wait.until(
-#     ec.element_to_be_clickable(
-#         inputa
-#     )
-# )
-#
-#
-#
-# sleep(1)
-# add.click()
-# add.send_keys("ELlo")
-# # print(add.get_attribute("outerHTML"))
-#
-# price_per_month = wait.until(
-#     ec.element_to_be_clickable(
-#         (By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-#     )
-# )
-#
-# price_per_month.click()
-# price_per_month.send_keys("wassappp")
-#
-#
-# link = wait.until(
-#     ec.element_to_be_clickable(
-#         (By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
-#     )
-# )
-#
-# link.click()
-# link.send_keys("wassappp")
-#
-# submit_btn = wait.until(
-#     ec.element_to_be_clickable(
-#         (By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
-#     )
-# )
-# submit_btn.click()
-all_link_elements = soup.select(".StyledPropertyCardDataWrapper a")
-all_links = [link["href"] for link in all_link_elements]
-print(f"There are {len(all_links)} links to individual listings in total: \n")
-print(all_links)
+# This keeps our browser open
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach",True)
+
+
+
+#The Driver
+driver = webdriver.Chrome(options=chrome_options)
+
+wait = WebDriverWait(driver, 2)
+for n in range(len(all_links)):
+    # TODO: Add fill in the link to your own Google From
+    driver.get(FORM_URL)
+
+    # Use the xpath to select the "short answer" fields in your Google Form.
+    # Note, your xpath might be different if you created a different form.
+    address = driver.find_element(by=By.XPATH,
+                                  value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element(by=By.XPATH,
+                                value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element(by=By.XPATH,
+                               value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element(by=By.XPATH,
+                                        value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+
+    address.send_keys(addy[n])
+    price.send_keys(prices[n])
+    link.send_keys(links[n])
+    submit_button.click()
