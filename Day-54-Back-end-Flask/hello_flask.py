1f5b3390ffe27bb5d3d9dce98ae31ff74b6d07c8
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-05 19:29:19 +0530

    THis is how we use routes

--- Day-54-Back-end-Flask/hello_flask.py
+++ Day-54-Back-end-Flask/hello_flask.py
@@ -1,32 +1,34 @@
 import time
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # This will be shown im your browser
+    return 'Hello, World!'
+
+@app.route('/bye')
+def say_bye():
+    return "Byeee!!!!"
+
+if __name__ == "__main__":
+    app.run()
+
+
+# def delay_decorator(function):
+#     def wrapper_function():
+#         time.sleep(2)
+#         #DO Something Before
+#         function()
+#         #Do Something After
+#     return wrapper_function
 #
-# @app.route('/')
-# def hello_world():
-#     # This will be shown im your browser
+# @delay_decorator
+# def say_hello():
+#     print("Hello World")
 #
+# def say_ola():
+#     print("Ola Amigos")
 #
-#     return 'Hello, World!'
-#
-# if __name__ == "__main__":
-#     app.run()
-
-
-def delay_decorator(function):
-    def wrapper_function():
-        time.sleep(2)
-        #DO Something Before
-        function()
-        #Do Something After
-    return wrapper_function
-
-@delay_decorator
-def say_hello():
-    print("Hello World")
-
-def say_ola():
-    print("Ola Amigos")
-
-say_hello()
-say_ola()
+# say_hello()
+# say_ola()
