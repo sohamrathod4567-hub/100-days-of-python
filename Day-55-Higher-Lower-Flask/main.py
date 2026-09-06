643a0be82a4e82ae3f7f0f60b046bde8e56820c2
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-06 14:31:15 +0530

    Initial Commit

---
+++ Day-55-Higher-Lower-Flask/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # This will be shown im your browser
+    return 'Hello, World!'
+
+@app.route('/bye')
+def say_bye():
+    return "Byeee!!!!"
+
+if __name__ == "__main__":
+    app.run()
