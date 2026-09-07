dd760e05f4fbe4ad96ee68c06cb03ef254b3e828
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-07 20:01:29 +0530

    This thing is working, tho some style is missing

--- Day-55-Higher-Lower-Flask/Final.py
+++ Day-55-Higher-Lower-Flask/Final.py
@@ -1,6 +1,10 @@
+from random import randint
+
 from flask import Flask
+import random
 app = Flask(__name__)
 
+GOAL = randint(0,9)
 
 @app.route('/')
 def hello_world():
@@ -9,6 +13,14 @@ def hello_world():
         '<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWh2cDJxMjlyZzFpNjkycHJnbGZ1cHo5ZTJhano4ZWNrMXd6b21kYyZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/ASd0Ukj0y3qMM/giphy.gif">'
     )
 
+@app.route('/<int:number>')
+def high_low(number):
+    if number > GOAL:
+        return f"You Are very high from goal"
+    elif number < GOAL:
+        return f"You Are very low from goal"
+    else:
+        return f"You Found mee Yaaayyyy!!!!"
 
 if __name__ == "__main__":
     app.run(debug=True)
