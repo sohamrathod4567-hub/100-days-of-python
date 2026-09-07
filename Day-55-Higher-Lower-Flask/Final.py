3433fc9f7b923cd0191717138d05d646da0d8a45
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-07 19:47:57 +0530

    It worked

--- Day-55-Higher-Lower-Flask/Final.py
+++ Day-55-Higher-Lower-Flask/Final.py
@@ -1,12 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    # This will be shown im your browser
-    return 'Hello, World!'
-
-
+    return (
+        "<h1>Guess the Number between 0 and 9</h1>"
+        '<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcWh2cDJxMjlyZzFpNjkycHJnbGZ1cHo5ZTJhano4ZWNrMXd6b21kYyZlcD12MV9naWZzX3RyZW5kaW5nJmN0PWc/ASd0Ukj0y3qMM/giphy.gif">'
+    )
 
 
 if __name__ == "__main__":
