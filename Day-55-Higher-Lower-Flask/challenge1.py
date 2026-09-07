67dad2c8f3a6ff962a88077ef5d5f77c9c4ac3fe
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-07 18:58:27 +0530

    Added decorator to make the text bold

--- Day-55-Higher-Lower-Flask/challenge1.py
+++ Day-55-Higher-Lower-Flask/challenge1.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+def bold_decorator(function):
+    def wrapper_function():
+        #DO Something Before
+        result = function()
+        return f"\033[1m{result}\033[0m"
+        #Do Something After
+    return wrapper_function
 
 
 
@@ -8,6 +15,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@bold_decorator
 def hello_world():
     # This will be shown im your browser
     return 'Hello, World!'
