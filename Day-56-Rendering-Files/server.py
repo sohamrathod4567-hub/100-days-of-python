4443704a8a74ad0a1d4e4c7a56e077c63fb6b261
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-08 16:46:18 +0530

    I was able to do it

--- Day-56-Rendering-Files/server.py
+++ Day-56-Rendering-Files/server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/pablo")
+def pablo():
+    return render_template("pablo.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
