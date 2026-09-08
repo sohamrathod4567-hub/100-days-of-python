360eb66fefaafbb073ad6d60049b7efea3b486e8
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-08 16:41:55 +0530

    Changed some code

--- Day-56-Rendering-Files/server.py
+++ Day-56-Rendering-Files/server.py
@@ -1,31 +1,11 @@
-# from flask import Flask , render_template
-#
-# app = Flask(__name__)
-#
-# @app.route("/")
-# def home():
-#     return render_template("index.html")
-#
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
-from flask import Flask, render_template
-import os
+from flask import Flask , render_template
 
 app = Flask(__name__)
-
-print("Root path:", app.root_path)
-print("Template folder:", app.template_folder)
-print(
-    "Index exists:",
-    os.path.exists(os.path.join(app.root_path, "templates", "index.html"))
-)
-
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
     app.run(debug=True)
+
