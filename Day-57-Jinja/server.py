8f097691fe9badd1538f006b2738985292b5ee27
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-11 15:37:26 +0530

    Got the current year

--- Day-57-Jinja/server.py
+++ Day-57-Jinja/server.py
@@ -1,12 +1,14 @@
 import random
+from datetime import datetime
 from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    current_year = datetime.now().year
     random_number = random.randint(1,10)
-    return render_template("index.html", num = random_number)
+    return render_template("index.html", num = random_number, year = current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
