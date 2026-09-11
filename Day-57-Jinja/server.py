e436ea346b15bb95a5b9f59c8a79be2a1a853931
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-11 15:32:32 +0530

    Added a random number in the HTML

--- Day-57-Jinja/server.py
+++ Day-57-Jinja/server.py
@@ -1,10 +1,12 @@
+import random
 from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    random_number = random.randint(1,10)
+    return render_template("index.html", num = random_number)
 
 if __name__ == "__main__":
     app.run(debug=True)
