c38d1fa5c0280614004c22fe03a354bbfd1a543f
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-11 17:37:31 +0530

    Initial commit for blogs

--- Day-57-Jinja/server.py
+++ Day-57-Jinja/server.py
@@ -18,5 +18,12 @@ def age(name):
     name = data["name"]
     old = data["age"]
     return render_template("index.html", name = name, old = old)
+
+@app.route('/blog')
+def blog():
+    blog_url = "https://www.npoint.io/docs/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts = all_posts)
 if __name__ == "__main__":
     app.run(debug=True)
