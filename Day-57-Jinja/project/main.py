088323b4eb78fb748168bd03e49ca8078a9d6a3b
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-12 19:03:42 +0530

    Got it to print the blogs

--- Day-57-Jinja/project/main.py
+++ Day-57-Jinja/project/main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/ecfe5890e4857ae9911f"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
