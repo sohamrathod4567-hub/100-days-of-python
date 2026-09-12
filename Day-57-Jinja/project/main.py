7bf9178c1a0bae49f7be199db0627e010e48f907
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-12 19:18:01 +0530

    Got everything to work properly

--- Day-57-Jinja/project/main.py
+++ Day-57-Jinja/project/main.py
@@ -11,5 +11,10 @@ def home():
     all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
+@app.route('/post/<id>')
+def get_post(id):
+    post_id = id
+    return render_template("post.html", post_id=post_id)
+
 if __name__ == "__main__":
     app.run(debug=True)
