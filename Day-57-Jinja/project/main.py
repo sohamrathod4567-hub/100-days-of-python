84a81790f7015c3fff405b443d0915488355e460
Soham Rathod <sohamrathod4567@gmail.com>
2026-09-12 19:27:10 +0530

    Made some changes to the approach

--- Day-57-Jinja/project/main.py
+++ Day-57-Jinja/project/main.py
@@ -11,10 +11,19 @@ def home():
     all_posts = response.json()
     return render_template("index.html", posts=all_posts)
 
-@app.route('/post/<id>')
+@app.route('/post/<int:id>')
 def get_post(id):
-    post_id = id
-    return render_template("post.html", post_id=post_id)
+    blog_url = "https://api.npoint.io/ecfe5890e4857ae9911f"
+    response = requests.get(blog_url)
+    all_posts = response.json()
 
+    requested_post = None
+
+    for post in all_posts:
+        if post["id"] == id:
+            requested_post = post
+            break
+
+    return render_template("post.html", post=requested_post)
 if __name__ == "__main__":
     app.run(debug=True)
